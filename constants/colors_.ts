454f910ed21d63bf31cc4902572c6b504b185fbc
judithmg <jmartigo33@gmail.com>
2022-05-28 12:18:18 +0200

    add colors consts

---
+++ constants/colors_.ts
@@ -0,0 +1,26 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+export const black = "#000";
+export const fog = "#F2F6F9";
+export const gray = "#B2B2B2";
+export const nightBlue = "#6979F8";
+export const stratos = "#00093E";
+export const white = "#fff";
+export const wool = "#171725";
+
+export default {
+  light: {
+    text: stratos,
+    background: fog,
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#fff",
+    background: "#000",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+  },
+};
